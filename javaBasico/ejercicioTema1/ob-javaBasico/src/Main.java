3bede68a0c4aa4baa6c95c9c2881cd5bf0d6ac4e
Waldemar Almeyda <argenisibrainwaldemar@gmail.com>
2023-06-14 17:39:45 -0500

    first commit

---
+++ javaBasico/ejercicioTema1/ob-javaBasico/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipo numericos
+        byte number1 = 1; //1 byte
+        short number2 = 2; //2 byte
+        int number3 = 3; //4 byte
+        long number4 = 4L; //8 byte
+
+        // Punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // Cadena y caracterest
+        char caracter1 = 'a';
+        String nombre = "Ibrahim";
+
+        // Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+
+        System.out.println(caracter1);
+        System.out.println(nombre);
+
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+
+    }
+}
