a92b3bb820e6d28a3b65db948d5ac9bc35582e3b
Waldemar Almeyda <argenisibrainwaldemar@gmail.com>
2023-06-15 11:40:36 -0500

    refactoring

--- javaBasico/ejercicioTema1/ejercicioTema1/src/com/example/Main.java
+++ javaBasico/ejercicioTema1/ejercicioTema1/src/com/example/Main.java
@@ -1,20 +1,21 @@
+package com.example;
+
 public class Main {
     public static void main(String[] args) {
-        // Tipo numericos
+        // Tipo de datos numericos
         byte number1 = 1; //1 byte
         short number2 = 2; //2 byte
         int number3 = 3; //4 byte
         long number4 = 4L; //8 byte
 
-        // Punto flotante
         float decimal1 = 4.9f;
         double decimal2 = 9.99d;
 
-        // Cadena y caracterest
-        char caracter1 = 'a';
+        // Tipo de datos String y caracteres
         String nombre = "Ibrahim";
+        char caracter1 = 'a';
 
-        // Booleanos
+        //Tipo de dato booleanos
         boolean verdadero = true;
         boolean falso = false;
 
@@ -26,12 +27,10 @@ public class Main {
         System.out.println(decimal1);
         System.out.println(decimal2);
 
-        System.out.println(caracter1);
         System.out.println(nombre);
+        System.out.println(caracter1);
 
         System.out.println(verdadero);
         System.out.println(falso);
-
-
     }
 }
