1713c17cee4eedd71e1751c40e8473d35c707ea8
Ibrahim Almeyda <83681536+Ibrahim-003@users.noreply.github.com>
2023-07-21 11:19:06 -0500

    Update Main.java

--- javaAvanzado/tema678/src/org/example/Main.java
+++ javaAvanzado/tema678/src/org/example/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Configuracion config1 = Configuracion.getInstance();
         Configuracion config2 = Configuracion.getInstance();
 
-        // Comprobando que ambas variables apunten a la misma instancia
-        System.out.println(config1 == config2);
+        // Comprobando que ambas variables apunten a la misma instancia 
+        System.out.println(config1 == config2); // true
 
         // Modificar la configuracion en una instancia afecta a la otra
         config1.setTema("Oscuro");
